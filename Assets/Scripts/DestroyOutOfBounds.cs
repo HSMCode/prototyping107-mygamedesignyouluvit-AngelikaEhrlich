6dcb0446e12a40658b120ae170a82bfed1e92222
Angelika Ehrlich <angelikaehrlich@gmx.net>
2021-01-06 18:00:35 +0100

    remove heart from the scene

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -17,25 +17,25 @@ public class DestroyOutOfBounds : MonoBehaviour
     void Update()
     {
         // If a penguin goes past the players view in  the game, remove that penguin
-        if (transform.position.z > topBound)
-        {
-            Destroy(gameObject);
-        }
+            if (transform.position.z > topBound)
+            {
+                Destroy(gameObject);
+            }
 
-        else if (transform.position.z < lowerBound)
-        {
-            //check to see if we have any lives left
-            if (KeepLives.lives != 0)
+            else if (transform.position.z < lowerBound)
             {
-                Destroy(gameObject);
-                KeepLives.lives -= 1;
+                //check to see if we have any lives left
+                if (KeepLives.lives != 0)
+                {
+                    Destroy(gameObject);
+                    KeepLives.lives -= 1;
+                }
+                else
+                {
+                    Destroy(gameObject);
+                    KeepLives.lives = 0;
+                }
             }
-            else
-            {
-                Destroy(gameObject);
-                KeepLives.lives = 0;
-            }
-        }
     }
 
 }
