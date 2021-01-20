cab24014243bb98a3042f5d1abe9c73bb4fb1fe3
Angelika Ehrlich <angelikaehrlich@gmx.net>
2021-01-20 13:03:11 +0100

    add hover and click sounds to buttons

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,12 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    AudioSource buttonsound;
-
-    private void Start()
-    {
-        buttonsound = GetComponent<AudioSource>();
-    }
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
